9c59767ac90193c559f87d6df1cc0c3fde8552fd
AnibalG <anibalgamboac@gmail.com>
2017-07-23 19:12:01 -0500

    servidor funcional aparentemente

--- servidor.c
+++ servidor.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
-#include <sys/socket.h>
 
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>    
+#include <sys/socket.h>    
+#include <fcntl.h>
+#include <unistd.h>
+#include <arpa/inet.h> 
 int main( int argc, char *argv[]){
 	if(argc == 1){
 		printf("Uso: ./servidor <numero de puerto>\n");
@@ -19,14 +24,26 @@ int main( int argc, char *argv[]){
 	server.sin_addr.s_addr = inet_addr("127.0.0.1") ;
 	server.sin_port = htons(puerto);
 	
-	bind(sd,(struct sockaddr *)&server , sizeof(server))
+	bind(sd,(struct sockaddr *)&server , sizeof(server));
 
-	listen(socket_desc , 10);
+	listen(sd , 10);
 
-	c = sizeof(struct sockaddr_in);
+	int c = sizeof(struct sockaddr_in);
      
-	newsd = accept(sd, (struct sockaddr *)&client, (socklen_t*)&c);
+	int newsd = accept(sd, (struct sockaddr *)&client, (socklen_t*)&c);
 	char *ruta = malloc(1024);
 	void *file = malloc(1024);
-	while( (read_size = recv(newsd, client_message , 2000 , 0)) > 0 ){
+	int read_size;
+	while( (read_size = recv(newsd, ruta, 2000 , 0)) > 0 ){
+	
+		int fd = open(ruta+4, O_RDONLY);
+		int filesize = read(fd, file, 1024);
+		write(newsd, file, filesize);
+		close(fd);
+	}
+			
+	close(newsd);  
+	
+	return(0);
+	
 }
